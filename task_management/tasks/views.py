0873a6040cfa774fa5447de2d2d9223a97eb79d8
Origina-sudo <arthurba8585@gmail.com>
2024-10-03 12:28:52 +0000

    Updates

--- task_management/tasks/views.py
+++ task_management/tasks/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Task
+from .serializers import TaskSerializer
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework import status
 
-# Create your views here.
+class TaskViewSet(viewsets.ModelViewSet):
+    serializer_class = TaskSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        return Task.objects.filter(user=self.request.user)
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
+    def update(self, request, *args, **kwargs):
+        task = self.get_object()
+        if task.status == 'Completed':
+            return Response({"error": "Completed tasks cannot be edited unless reverted."}, status=status.HTTP_400_BAD_REQUEST)
+        return super().update(request, *args, **kwargs)
