ede0060bb6e4c2fea03d9084c52b2e6ff200c599
Origina-sudo <arthurba8585@gmail.com>
2024-10-03 12:18:14 +0000

    Updates

--- task_management/task_management/settings.py
+++ task_management/task_management/settings.py
@@ -76,9 +76,13 @@ WSGI_APPLICATION = "task_management.wsgi.application"
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+     'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'task_management_db',  # Name your database
+        'USER': 'Orinia_sudo', 
+        'PASSWORD': 'Asphalt1234500#@',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
