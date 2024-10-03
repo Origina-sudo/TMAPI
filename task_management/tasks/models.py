2bbc8bb07053e1ba99194aea514c95e10ad21608
Origina-sudo <arthurba8585@gmail.com>
2024-10-03 12:19:12 +0000

    Updates

--- task_management/tasks/models.py
+++ task_management/tasks/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Task(models.Model):
+    STATUS_CHOICES = [
+        ('Pending', 'Pending'),
+        ('Completed', 'Completed'),
+    ]
+
+    PRIORITY_CHOICES = [
+        ('Low', 'Low'),
+        ('Medium', 'Medium'),
+        ('High', 'High'),
+    ]
+
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    due_date = models.DateField()
+    priority = models.CharField(max_length=6, choices=PRIORITY_CHOICES)
+    status = models.CharField(max_length=8, choices=STATUS_CHOICES, default='Pending')
+    created_at = models.DateTimeField(auto_now_add=True)
+    completed_at = models.DateTimeField(null=True, blank=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
